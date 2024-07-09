6ad97f5eb22a105973fd3b5e3a1bcda86d798d3b
coder <147401359+Karthiknekraje@users.noreply.github.com>
2024-07-09 07:26:49 +0530

    Update crossword.py

--- crossword.py
+++ crossword.py
@@ -6,39 +6,35 @@ class Crossword:
     def display(self):
         for row in self.grid:
             print(' '.join(row))
-        print()
     
-    def can_place_word_horizontally(self, word, row, col):
-        if col + len(word) > self.size:
-            return False
-        return all(self.grid[row][col + i] in (' ', word[i]) for i in range(len(word)))
+    def can_place_word(self, word, row, col, horizontal=True):
+        if horizontal:
+            if col + len(word) > self.size: return False
+            return all(self.grid[row][col + i] in (' ', word[i]) for i in range(len(word)))
+        else:
+            if row + len(word) > self.size: return False
+            return all(self.grid[row + i][col] in (' ', word[i]) for i in range(len(word)))
     
-    def can_place_word_vertically(self, word, row, col):
-        if row + len(word) > self.size:
-            return False
-        return all(self.grid[row + i][col] in (' ', word[i]) for i in range(len(word)))
-    
-    def place_word_horizontally(self, word, row, col):
+    def place_word(self, word, row, col, horizontal=True):
         for i in range(len(word)):
-            self.grid[row][col + i] = word[i]
-    
-    def place_word_vertically(self, word, row, col):
-        for i in range(len(word)):
-            self.grid[row + i][col] = word[i]
+            if horizontal:
+                self.grid[row][col + i] = word[i]
+            else:
+                self.grid[row + i][col] = word[i]
     
     def add_word(self, word):
         for row in range(self.size):
             for col in range(self.size):
-                if self.can_place_word_horizontally(word, row, col):
-                    self.place_word_horizontally(word, row, col)
+                if self.can_place_word(word, row, col, True):
+                    self.place_word(word, row, col, True)
                     return True
-                if self.can_place_word_vertically(word, row, col):
-                    self.place_word_vertically(word, row, col)
+                if self.can_place_word(word, row, col, False):
+                    self.place_word(word, row, col, False)
                     return True
         return False
 
 def main():
-    size = 10  # Size of the crossword grid
+    size = 10
     words = ['python', 'java', 'ruby', 'javascript', 'html', 'css']
     
     crossword = Crossword(size)
